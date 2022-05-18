bfb479370cbc57d74ec1c90a4a594db01b8187f4
koeitsu <brun0zalc@hotmail.com>
2022-05-18 09:03:51 -0300

    gira cubos

---
+++ funcs.py
@@ -0,0 +1,7 @@
+import math
+
+def advance(lines, angle, amt):
+    new_lines = lines.copy()
+    new_lines[0] += math.cos(math.radians(angle)) * amt
+    new_lines[1] += math.sin(math.radians(angle)) * amt
+    return new_lines
