030de5ff088f793064b5cd2daa83922d9ec60898
koeitsu <brun0zalc@hotmail.com>
2022-05-18 09:04:33 -0300

    background

--- level1.py
+++ level1.py
@@ -1,10 +1,15 @@
-import pygame
+import pygame, math, random
 from config import *
 from assets import *
 from classes import *
+from funcs import *
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+square_effects = []
+player_pos = [window.get_width() // 2, window.get_height() // 2]
 
 def level1(window):
-
+    scrolling = 0
     clock = pygame.time.Clock()
 
     assets = load_assets()
@@ -43,10 +48,32 @@ def level1(window):
 
         if player.is_on_wall == False:
             player.GRAVITY += 1
+        
+        if scrolling < player_pos[1] - 200:
+            scrolling += (player_pos[1] - 200 - scrolling) / 10
 
         all_sprites.update()
 
-        window.fill(WHITE)
+        window.fill((background_color))
+
+        if random.randint(1, 60) == 1:
+            square_effects.append([[random.randint(0, window.get_width()), -80 + scrolling], random.randint(0, 359), random.randint(10, 30) / 20, random.randint(15, 40), random.randint(10, 30) / 500])
+
+        for i, effect in sorted(enumerate(square_effects), reverse = True):
+            effect[0][1] += effect[2]
+            effect[1] += effect[2] * effect[4]
+            effect[3] -= effect[4]
+            points = [
+                gira(effect[0], math.degrees(effect[1]), effect[3]),
+                gira(effect[0], math.degrees(effect[1]) + 90, effect[3]),
+                gira(effect[0], math.degrees(effect[1]) + 180, effect[3]),
+                gira(effect[0], math.degrees(effect[1]) + 270, effect[3]),
+            ]
+            if effect[3] < 1:
+                square_effects.pop(i)
+            else:
+                pygame.draw.polygon(window, background_polygon_color, points, 2)
+       
         all_sprites.draw(window)
 
         pygame.display.update()
