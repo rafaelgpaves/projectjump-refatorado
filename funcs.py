51d0d9853aff670786ac216f8c70bd9c975fa59d
koeitsu <brun0zalc@hotmail.com>
2023-03-14 11:03:11 -0300

    extraindo método para os cubos

--- funcs.py
+++ funcs.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, random
 import numpy as np
 
 def gira(lines, angle, amount):
@@ -6,3 +6,22 @@ def gira(lines, angle, amount):
     new_lines[0] += math.cos(math.radians(angle)) * amount
     new_lines[1] += math.sin(math.radians(angle)) * amount
     return new_lines
+
+def draw_cubes(window, background_polygon_color, cube_scroll, square_effects):
+    if random.randint(1, 60) == 1:
+        square_effects.append([[random.randint(0, window.get_width()), - 90 + cube_scroll], random.randint(0, 359), random.randint(10, 30) / 20, random.randint(15, 50), random.randint(10, 40) / 500])
+
+    for i, effect in sorted(enumerate(square_effects), reverse = True):
+        effect[0][1] += effect[2]
+        effect[1] += effect[2] * effect[4]
+        effect[3] -= effect[4] / 2
+        points = [
+                gira(effect[0], math.degrees(effect[1]), effect[3]),
+                gira(effect[0], math.degrees(effect[1]) + 90, effect[3]),
+                gira(effect[0], math.degrees(effect[1]) + 180, effect[3]),
+                gira(effect[0], math.degrees(effect[1]) + 270, effect[3]),
+            ]
+        if effect[3] < 1:
+            square_effects.pop(i)
+        else:
+            pygame.draw.polygon(window, background_polygon_color, points, 2)
--- level2.py
+++ level2.py
@@ -133,23 +133,7 @@ def level2(window, dificuldade):
         all_sprites.update()
 
         # Cubos!
-        if random.randint(1, 60) == 1:
-            square_effects.append([[random.randint(0, window.get_width()), - 90 + cube_scroll], random.randint(0, 359), random.randint(10, 30) / 20, random.randint(15, 50), random.randint(10, 40) / 500])
-
-        for i, effect in sorted(enumerate(square_effects), reverse = True):
-            effect[0][1] += effect[2]
-            effect[1] += effect[2] * effect[4]
-            effect[3] -= effect[4] / 2
-            points = [
-                gira(effect[0], math.degrees(effect[1]), effect[3]),
-                gira(effect[0], math.degrees(effect[1]) + 90, effect[3]),
-                gira(effect[0], math.degrees(effect[1]) + 180, effect[3]),
-                gira(effect[0], math.degrees(effect[1]) + 270, effect[3]),
-            ]
-            if effect[3] < 1:
-                square_effects.pop(i)
-            else:
-                pygame.draw.polygon(window, background_polygon_color, points, 2)
+        draw_cubes(window, background_polygon_color, cube_scroll, square_effects)
 
         all_sprites.draw(window)
 
